1c5ef5bf443eae2ddaf050246243d0e6b7619e5f
Paalamugan <42642576+paalamugan@users.noreply.github.com>
2024-05-21 11:35:33 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,6 +12,15 @@ const authMiddleware = withAuth(
   function onSuccess(_req) {
     // you can get a token from the request object(req.nextauth.token) and do something with it
     // In here, do any kind of logging or tracking here or redirect to a custom URL
+  },
+  {
+    callbacks: {
+      authorized: async ({ token }) => {
+        // If a user is authenticated, the token will be present
+        // and the user is authorized.
+        return !!token?.id;
+      },
+    },
   }
 );
 
